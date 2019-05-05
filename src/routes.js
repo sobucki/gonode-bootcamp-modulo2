1a9cc21ef0a121f9fa05e4a8b972d461c0262e0e
sobucki <sobucki@gmail.com>
2019-05-05 19:29:07 -0400

    Concluindo agendamento

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,8 @@ const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
+const AppointmentController = require('./app/controllers/AppointmentController')
+const AvailableController = require('./app/controllers/AvailableController')
 
 // middleware para deixar em uma variavel globlal a todas views do nunjuks
 routes.use((req, res, next) => {
@@ -35,5 +37,10 @@ routes.use('/app', authMiddleware)
 routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', DashboardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
+routes.post('/app/appointments/new/:provider', AppointmentController.store)
+
+routes.get('/app/available/:provider', AvailableController.index)
 
 module.exports = routes
